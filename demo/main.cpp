226ea229ec62e13f8582b91d8bd989a0c504c864
xukq <xu_kangqi@mail.dlut.edu.cn>
2023-12-17 21:35:43 +0800

    tunnel MIPMAP

--- demo/main.cpp
+++ demo/main.cpp
@@ -12,132 +12,89 @@
 
 class my_application : public sb7::application {
 public:
-    my_application() : tex_index{0} {
-    }
-
     void render(double currentTime) override {
-        static const GLfloat gray[] = {0.2f, 0.2f, 0.2f, 1.0f};
-        static const GLfloat ones[] = {1.0f};
-
-        glClearBufferfv(GL_COLOR, 0, gray);
-        glClearBufferfv(GL_DEPTH, 0, ones);
+        static const GLfloat black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+        float t = (float) currentTime;
 
         glViewport(0, 0, info.windowWidth, info.windowHeight);
-
-        // glBindTexture(GL_TEXTURE_2D, tex_object[tex_index]);
-        glBindTextureUnit(0, tex_object[tex_index]);
+        glClearBufferfv(GL_COLOR, 0, black);
 
         glUseProgram(program);
 
-        vmath::mat4 proj_matrix = vmath::perspective(60.0f, (float) info.windowWidth / (float) info.windowHeight, 0.1f,
-                                                     1000.0f);
-        vmath::mat4 mv_matrix = vmath::translate(0.0f, 0.0f, -3.0f) *
-                                vmath::rotate((float) currentTime * 19.3f, 0.0f, 1.0f, 0.0f) *
-                                vmath::rotate((float) currentTime * 21.1f, 0.0f, 0.0f, 1.0f);
+        vmath::mat4 proj_matrix = vmath::perspective(60.0f,
+                                                     (float) info.windowWidth / (float) info.windowHeight,
+                                                     0.1f, 1000.0f);
 
-        glUniformMatrix4fv(uniforms.mv_matrix, 1, GL_FALSE, mv_matrix);
-        glUniformMatrix4fv(uniforms.proj_matrix, 1, GL_FALSE, proj_matrix);
+        glUniform1f(uniforms.offset, t * 0.003f);
 
-        object.render();
+
+        GLuint textures[] = {tex_wall, tex_floor, tex_wall, tex_ceiling};
+        for (int i = 0; i < 4; ++i) {
+            vmath::mat4 mv_matrix =
+                    vmath::rotate(90.0f * (float) i, vmath::vec3(0.0f, 0.0f, 1.0f)) *
+                    vmath::translate(-0.5f, 0.0f, -10.0f) *
+                    vmath::rotate(90.0f, 0.0f, 1.0f, 0.0f) *
+                    vmath::scale(30.0f, 1.0f, 1.0f);
+            vmath::mat4 mvp = proj_matrix * mv_matrix;
+
+            glUniformMatrix4fv(uniforms.mvp, 1, GL_FALSE, mvp);
+
+            glBindTextureUnit(0, textures[i]);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
     }
 
     void startup() override {
-#define B 0x00, 0x00, 0x00, 0x00
-#define W 0xFF, 0xFF, 0xFF, 0xFF
-        static const GLubyte tex_data[] =
-        {
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-            B, W, B, W, B, W, B, W, B, W, B, W, B, W, B, W,
-            W, B, W, B, W, B, W, B, W, B, W, B, W, B, W, B,
-        };
-#undef B
-#undef W
-
-        glCreateTextures(GL_TEXTURE_2D, 1, tex_object);
-        glBindTextureUnit(0, tex_object[0]);
-        glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB8, 16, 16);
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 16, 16, GL_RGBA, GL_UNSIGNED_BYTE, tex_data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        sp_program.reset(new Demo::Shader_Program());
+        sp_program->compile(GL_VERTEX_SHADER)
+                .compile(GL_FRAGMENT_SHADER)
+                .link();
+        program = sp_program->get();
 
 
-        tex_object[1] = sb7::ktx::file::load("media/textures/pattern1.ktx");
-
-        object.load("media/objects/torus_nrms_tc.sbm"); {
-            sp_program.reset(new Demo::Shader_Program());
-            sp_program->compile(GL_VERTEX_SHADER)
-                    .compile(GL_FRAGMENT_SHADER)
-                    .link();
-            program = sp_program->get();
-        }
-
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
+        // glEnable(GL_DEPTH_TEST);
+        // glDepthFunc(GL_LEQUAL);
 
 
-        uniforms.mv_matrix = glGetUniformLocation(program, "mv_matrix");
-        uniforms.proj_matrix = glGetUniformLocation(program, "proj_matrix");
-    }
+        uniforms.mvp = glGetUniformLocation(program, "mvp");
+        uniforms.offset = glGetUniformLocation(program, "offset");
 
-    virtual void onKey(int key, int action) override {
-        if (action) {
-            switch (key) {
-                case 'T':
-                    tex_index++;
-                    if (tex_index > 1)
-                        tex_index = 0;
-                    break;
-            }
+        glCreateVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        tex_wall = sb7::ktx::file::load("media/textures/brick.ktx");
+        tex_ceiling = sb7::ktx::file::load("media/textures/ceiling.ktx");
+        tex_floor = sb7::ktx::file::load("media/textures/floor.ktx");
+
+        GLuint textures[] = {tex_floor, tex_wall, tex_ceiling};
+
+        for (int i = 0; i < 3; ++i) {
+            glBindTextureUnit(0, textures[i]);
+            glTextureParameteri(textures[i], GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTextureParameteri(textures[i], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         }
     }
+
 
     void shutdown() override {
         glDeleteProgram(program);
-        glDeleteTextures(2, tex_object);
     }
 
-private:
-    void generate_texture(float* data, int width, int height) {
-        for (int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                data[(y * width + x) * 4 + 0] = (float) (x & y & 0XFF) / 255.0f;
-                data[(y * width + x) * 4 + 1] = (float) (x | y & 0XFF) / 255.0f;
-                data[(y * width + x) * 4 + 2] = (float) (x ^ y & 0XFF) / 255.0f;
-                data[(y * width + x) * 4 + 3] = 1.0f;
-            }
-        }
-    }
 
 private:
-    GLuint render_prog;
-
-    GLuint tex_object[2];
-    GLuint tex_index;
-
     struct {
-        GLint mv_matrix;
-        GLint proj_matrix;
+        GLint mvp;
+        GLint offset;
     } uniforms;
 
-    sb7::object object;
-
 private:
-    GLuint program;
     GLuint vao;
+    GLuint program;
     std::shared_ptr<Demo::Shader_Program> sp_program;
+
+    GLuint tex_wall;
+    GLuint tex_ceiling;
+    GLuint tex_floor;
 };
 
 DECLARE_MAIN(my_application);
--- demo/vs.glsl
+++ demo/vs.glsl
@@ -1,20 +1,21 @@
 #version 450 core
 
-uniform mat4 mv_matrix;
-uniform mat4 proj_matrix;
-
-layout(location = 0) in vec4 position;
-layout(location = 4) in vec2 tc;
-
 out VS_OUT
 {
     vec2 tc;
 } vs_out;
 
+uniform mat4 mvp;
+uniform float offset;
+
 void main(void) {
-    vec4 pos_vs = mv_matrix * position;
+    const vec2[4] position = vec2[4](
+        vec2(-0.5, -0.5),
+        vec2(0.5, -0.5),
+        vec2(-0.5, 0.5),
+        vec2(0.5, 0.5)
+    );
 
-    vs_out.tc = tc;
-
-    gl_Position =  proj_matrix * pos_vs;
+    vs_out.tc = (position[gl_VertexID].xy + vec2(offset, 0.5)) * vec2(30.0, 1.0);
+    gl_Position =  mvp * vec4(position[gl_VertexID], 0.0, 1.0);
 }
